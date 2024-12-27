d5c1ed94c2cb63edd38f182f56efe9d784578c6d
raonrabbit <raonrabbit@gmail.com>
2024-12-27 17:46:41 +0900

    fix: router-react-dom version issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider></RouterProvider>);
+root.render(<RouterProvider router={router} />);
