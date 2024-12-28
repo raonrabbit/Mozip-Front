ee3bc34dced5941ec5917651e44d33c93552aad9
dbswlrla112@naver.com <dbswlrla112@naver.com>
2024-12-28 19:19:12 +0900

    chore: change single to double quote rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<RouterProvider router={router} />);
