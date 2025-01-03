bcc34f19d5518eca06145214165bb06462ae915f
raonrabbit <raonrabbit@gmail.com>
2025-01-03 11:01:34 +0900

    feat: set up redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import router from "./router";
 import "./index.css";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<RouterProvider router={router} />);
+root.render(
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>,
+);
