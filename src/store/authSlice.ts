ccf1e9c1b0154d99d2da3d105f5cea58c59a4989
raonrabbit <raonrabbit@gmail.com>
2025-01-12 18:28:34 +0900

    feat: add redux slice for authentication

---
+++ src/store/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+	profileImg: string | null;
+	name: string | null;
+	email: string | null;
+	role: string | null;
+}
+
+interface AuthState {
+	isLoggedIn: boolean;
+	user: User | null;
+}
+
+const initialState: AuthState = {
+	isLoggedIn: false,
+	user: null,
+};
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		login: (state, action: PayloadAction<User>) => {
+			state.isLoggedIn = true;
+			state.user = action.payload;
+		},
+		logout: (state) => {
+			state.isLoggedIn = false;
+			state.user = null;
+		},
+	},
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
